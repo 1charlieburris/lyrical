3cf0e8f2c653dcc459b5a01a3473f469c7088c8f
Daniel Frederick <frederickd5@udayton.edu>
2024-02-17 23:56:48 -0500

    Prettier Formatting doing its work

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
