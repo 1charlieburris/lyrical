8375d108d30407608e02c32d9cc5d8631f20f39c
Daniel Frederick <frederickd5@udayton.edu>
2024-03-04 22:06:07 -0500

    Added onSubmit method to get input from the user. Did lots of styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SearchComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SearchComponent,
+    FormsModule,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,23 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  title!: string;
+  artist!: string;
 
+  ngOnInit(): void {}
+
+  async onSubmit() {
+    console.log(
+      'The user inputed the song: ' + this.title + ' by ' + this.artist
+    );
+  }
 }
