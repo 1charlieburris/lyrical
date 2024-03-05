a7d9134bb6c3cba26007c67c8959ef928f8e9dfe
Daniel Frederick <frederickd5@udayton.edu>
2024-03-04 20:39:57 -0500

    Added Search Component and added some basic styling and text boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
